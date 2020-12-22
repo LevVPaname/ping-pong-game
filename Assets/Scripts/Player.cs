0d0c3c85cb683288d6734f76d21ef33fd7fcf052
Vladislava <v.keturka@visionlabs.ru>
2020-12-22 20:49:09 +0300

    add player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  public Rigidbody2D Rigidbody2D;
+  [SerializeField] private int m_JumpPower = 10;
+  void Start()
+  {
+    //   Rigidbody2D.gravityScale = -1;
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      Debug.Log("Jump");
+      Rigidbody2D.velocity = new Vector2(0, m_JumpPower);
+    }
+  }
+}
